14cb42e86c6efef910af906afe3a85c708f11a2f
Stephen Kim <32920957+stephenk98@users.noreply.github.com>
2025-03-03 18:01:16 -0500

    Allow Front End to access backend

--- dummy_fi/dummy_fi/settings.py
+++ dummy_fi/dummy_fi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'dummy_fi_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
 ]
 
 ROOT_URLCONF = 'dummy_fi.urls'
